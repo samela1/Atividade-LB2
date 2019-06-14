fb301ed0c1fa457d2cd0b695f91202cbb362470c
samela1 <sameladejesus@gmail.com>
2019-06-14 19:40:44 -0300

    Sua mensagem, agora tem que da certo Samela

---
+++ atividade3.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace atividade3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double IMC, altura, peso;
+            Console.WriteLine("Digite sua altura?");
+            peso = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite seu peso?");
+            altura = Double.Parse(Console.ReadLine());
+            IMC = peso / altura;
+            Console.WriteLine(" índice é: " + IMC);
+
+
+
+        }
+    }
+}
